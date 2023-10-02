634caafe4c70609db07f39d9a4fbe14154f29d10
Isaac Turner <spacey_sooty@outlook.com>
2023-10-02 12:25:38 +0800

    Hello World program

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn hello_world() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/hello", routes![hello_world])
+}
